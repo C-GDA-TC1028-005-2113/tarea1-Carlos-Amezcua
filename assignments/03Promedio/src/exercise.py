088c8150cc0956cdfec59359ffcbade0306839aa
Carlos Amezcua <A01641742@itesm.mx>
2021-08-21 03:38:55 +0000

    Envío 1

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
-    #escribe tu código abajo de esta línea
+    sumaCalifs = 0
+    
+    sumaCalifs += float(input("Calificación de la materia: "))
+    sumaCalifs += float(input("Calificación de la materia: "))
+    sumaCalifs += float(input("Calificación de la materia: "))
+    sumaCalifs += float(input("Calificación de la materia: "))
 
+    promedio = sumaCalifs / 4
+
+    print(f"El promedio es: {promedio}")
 
 if __name__ == '__main__':
     main()
